deece35f0f1e96c6dd3300229ed917293d84a360
SuedesCarletti <suedes.carletti.tp@gmail.com>
2024-10-15 16:33:12 -0300

    Tentativa de Inicialização

--- wa_quickstart/start.js
+++ wa_quickstart/start.js
@@ -1,21 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log('WA_PHONE_NUMBER_ID:', process.env.WA_PHONE_NUMBER_ID);
-console.log('CLOUD_API_ACCESS_TOKEN:', process.env.CLOUD_API_ACCESS_TOKEN);
-console.log('CLOUD_API_VERSION:', process.env.CLOUD_API_VERSION);
-
 import WhatsApp from 'whatsapp';
 
-// Your test sender phone number
-const wa = new WhatsApp(process.env.WA_PHONE_NUMBER_ID);
-
-// Enter the recipient phone number
-const recipient_number = '5544984146379';
+const wa = new WhatsApp({
+    id: process.env.WA_PHONE_NUMBER_ID,
+    token: process.env.CLOUD_API_ACCESS_TOKEN,
+    version: process.env.CLOUD_API_VERSION
+});
 
 async function send_message() {
     try {
-        const sent_text_message = await wa.messages.text({ "body": "Hello world" }, recipient_number);
+        const sent_text_message = await wa.messages.text({
+            to: '5544984146379',
+            body: 'Hello world'
+        });
         console.log(sent_text_message.rawResponse());
     } catch (e) {
         console.log(JSON.stringify(e));
